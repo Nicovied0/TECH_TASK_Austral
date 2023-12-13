1cc23c15941aeb5fdd8461f2205fd732ed93a747
Nicovied0 <briannoviedo@gmail.com>
2023-12-13 18:59:30 -0300

    add more styles and progressive bar to stats

--- client/src/app/components/accordion/accordion.component.ts
+++ client/src/app/components/accordion/accordion.component.ts
@@ -6,15 +6,23 @@ import { Component, OnInit, Input } from '@angular/core';
   styleUrls: ['./accordion.component.scss'],
 })
 export class AccordionComponent implements OnInit {
-  @Input() data :any;
+  @Input() data: any;
 
   constructor() { }
 
   ngOnInit() { }
   showContent: boolean = false;
+  showContentEvolution: boolean = false;
 
   toggleAccordion() {
     this.showContent = !this.showContent;
+    this.showContentEvolution = false
+    console.log(this.data)
+  }
+
+  toggleAccordion2() {
+    this.showContentEvolution = !this.showContentEvolution;
+    this.showContent = false
     console.log(this.data)
   }
 
